16a9a3a993e56153fca88627461ebd8580d2d349
matt <matt@hru.sh>
2020-04-04 16:05:08 -0500

    Added in new statistics metrics

--- functions/query/utils.js
+++ functions/query/utils.js
@@ -2,6 +2,7 @@
 
 // Static Data
 const data_census = require('../../static/us_census.json')
+const data_geos = require('../../static/us_geohashes.json')
 
 /**
   * Formats the results based on the "type" specified by Grafana (table | timeseries)
@@ -17,10 +18,10 @@ module.exports.formatResults = (state, targets, target_data) => {
 
     // Re-format the data into the JSON data source structure
     targets.forEach(t => {
-                    
+
         const { target, type, data } = t
 
-        if(type == 'timeseries') {
+        if (type == 'timeseries') {
             results.push({
                 target: target,
                 datapoints: target_data[target]
@@ -30,15 +31,21 @@ module.exports.formatResults = (state, targets, target_data) => {
                 columns: [{
                     text: target,
                     type: "number"
-                },{
+                }, {
                     text: "Time",
                     type: "time"
                 }],
                 rows: target_data[target],
-                type: "table"
+                type: "table",
+                name: target,
+                tags: {
+                    geohash: data_geos[state]
+                }
             })
         }
     })
+
+    console.log(results)
 
     return results
 }
@@ -55,17 +62,35 @@ module.exports.computeMetric = (state, target, data) => {
 
     const sc_data = data_census[state]
 
-    if(target == 'density' || target == 'population'){
+    if (target == 'density' || target == 'population') {
         return sc_data[target]
     } else if (target == 'testsPerThousand') {
-
         const n_ttr = data.totalTestResults
         const n_pop = sc_data.population
-
         const tpc = n_ttr / (n_pop / 1000)
 
         return tpc
-    } 
+    } else if (target == 'prevalenceRate') {
+
+        const n_positive = data.positive
+        const n_population = sc_data.population
+
+        return n_positive / n_population
+
+    } else if (target == 'incidenceRate') {
+
+        const n_newPositive = data.positiveIncrease
+        const n_population = sc_data.population
+
+        return (n_newPositive / n_population) * 1000
+
+    } else if (target == 'caseFatalityRate') {
+
+        const n_deaths = data.death
+        const n_positive = data.positive
+
+        return (n_deaths / n_positive)
+    }
 
     console.log('Unable to compute unknown metric', target)
     return null
